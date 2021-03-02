44c719c59dc6dd14ee7725b8891b4767a9e7a114
georgecatalin <george.calin@gmail.com>
2021-03-02 17:33:17 +0200

    Declaring and initializing variables in C++

---
+++ Section6/DeclaringAndInitializingVariables/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int room_width;
+    int room_length;
+    
+    cout<<"Enter the width of the room:"<<endl;
+    cin>>room_width;
+    
+    cout<<"Enter the length of the room:"<<endl;
+    cin>>room_length;
+    
+    cout<<"The area of the room is "<<room_length*room_width<<" square meters."<<endl;
+    
+    return 0;
+}
