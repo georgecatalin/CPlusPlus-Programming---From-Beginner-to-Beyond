49327525587e285ed57939d858fe6eace09e6507
georgecatalin <george.calin@gmail.com>
2021-03-02 15:51:06 +0200

    Using namespaces in C++.

---
+++ Section5/UsingNamespaces/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    int my_favorite_number;
+    
+    cout<<"Enter your favourite number between 1 and 100: "<<endl;
+    cin>>my_favorite_number;
+    cout<<"Amazing!! That's my favorite number too!"<<endl;
+    cout<<"No really!!"<<my_favorite_number<<" is my favorite number."<<endl;
+    
+    
+    return 0;
+}
