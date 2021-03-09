4b541e6efb2e12ce1264f619826e661488fdc701
georgecatalin <george.calin@gmail.com>
2021-03-09 16:26:15 +0200

    Functions Prototypes in C++

---
+++ Section11_Functions/Functions_Prototypes/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+const double pi=3.14159;
+
+//Function prototypes
+double calculate_aria_circle(double radius);
+double calculate_volume_cylinder(double radius, double height);
+void area_circle();
+void volume_cylinder();
+
+int main()
+{
+    area_circle();
+    volume_cylinder();
+    
+    return 0;
+}
+
+
+double calculate_aria_circle(double radius)
+{
+    return pi*radius*radius;
+}
+
+void area_circle()
+{
+    double radius {};
+    
+    cout<<"\nEnter the radius of the circle : ";
+    cin>>radius;
+    
+    cout<<"\nThe area of a circle that has the radius of "<<radius<<" is : "<<calculate_aria_circle(radius)<<endl;
+    
+}
+
+double calculate_volume_cylinder(double radius, double height)
+{
+    return calculate_aria_circle(radius)*height;
+}
+
+void volume_cylinder()
+{
+    double radius {};
+    double height {};
+    
+    cout<<"\nEnter the radius of the cylinder: ";
+    cin>>radius;
+    
+    cout<<"\nEnter the height of the cylinder: ";
+    cin>>height;
+    
+    cout<<"\nThe Volume of the cylinder with a radius of "<<radius<<" and a height of "<<height<<" is "<<calculate_volume_cylinder(radius,height)<<endl;
+}
