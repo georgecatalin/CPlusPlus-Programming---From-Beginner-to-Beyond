ee8b5a807398c1a026dd76fd823c7ad3a5b79644
georgecatalin <george.calin@gmail.com>
2021-03-30 13:51:49 +0300

    Files, Streams and IO

---
+++ Section19_IO_and_Streams/Files_Streams_IO/main.cpp
@@ -0,0 +1,11 @@
+//FILES, STREAMS AND IO
+
+/*
+
+C++ provides a stream abstraction to work with IO devices. A stream is independent to the type of device. A stream is a sequence of bytes.
+
+ C++ provides different types of streams:
+ - Input stream provides data to the program from the input device
+ - Output stream receives data from the program and sends it to the output device
+ 
+ */
